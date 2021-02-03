d4d40aef855aaab510a4450804cfbdb0e90e04e4
jane-jianfangzhang <jane.zhangjianfang@gmail.com>
2021-02-03 13:16:56 +1100

    test

---
+++ working with data demo.R
@@ -0,0 +1,61 @@
+# 1. working with file base data - data import
+
+#Set the working directory that contains the data file
+setwd("/Users/jianfang.zhang/Documents/project_info/R-Data/r-demo")
+
+#Import/Load  the data into R-Read a tab-delimited data file
+cars <- read.table(
+  file = "Cars.txt",
+  header = TRUE,
+  sep = "\t",
+  quote = "\""
+)
+# Peek at the data
+head(cars)
+
+#2. Transform the data
+#Load library from memory
+library(dplyr)
+
+#Select a subset of columns by column name and assign the results of this transformation to variable temp
+temp <- select(
+  .data = cars,
+  Transmission,
+  Cylinders,
+  Fuel.Economy 
+)
+head(temp)
+
+# Filter data to include only cars with Automatic tranbsimisions
+# Filter a subset of rows(multiple condition )
+temp <- filter(
+  .data = temp,
+  Transmission == "Automatic",
+  Fuel.Economy > 20
+)
+head(temp)
+
+#Compute a new column, overwrite existing values or create new column
+temp <- mutate(
+  #dataframe
+  .data = temp,
+  #Name of the new column
+  Consumption = Fuel.Economy *0.425
+)
+
+head(temp)
+
+#Group rows of data by number of engine(Group by a column) 
+
+temp <- group_by(
+  .data = temp,
+  Cylinders
+)
+# return a group data frame
+head(temp)
+temp <- summarise(
+  .data = temp,
+  Avg.Consumption = mean(Consumption)
+)
+
+head(temp)
